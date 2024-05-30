8832aafeb84dc444f647c20e0fb63784f59bf98a
Aaryan Daiv <aaryandaiv@gmail.com>
2024-05-30 22:45:34 +0530

    changes in PeopleSkeleton

--- client/src/skeleton/PeopleSkeleton.jsx
+++ client/src/skeleton/PeopleSkeleton.jsx
@@ -4,7 +4,7 @@ const PeopleSkeleton = () => {
   return (
     <div>
       <div className="flex gap-4 items-center">
-        <div className="skeleton w-16 h-16 rounded-full shrink-0"></div>
+        <div className="skeleton w-12 h-12 rounded-full shrink-0"></div>
         <div className="flex flex-col gap-4">
           <div className="skeleton h-4 w-28"></div>
         </div>
